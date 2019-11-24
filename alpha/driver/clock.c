042e28eb292554a0018fa888a7cbfe7f84374f47
yuanle <1614903252@qq.com>
2019-11-24 16:32:23 +0800

    Add clock init function

--- alpha/driver/clock.c
+++ alpha/driver/clock.c
@@ -4,27 +4,74 @@
 #include <MCIMX6Y2.h>
 #include "clock.h"
 
-/*
-static void clock_init(void)
+
+static void system_clock_init(void)
 {
-    ze_u32_t reg = 0;
+    // switch pll1_sw_clk to step clock
+    CCM->CCSR &= ~(1 << 8);
+    CCM->CCSR |= (1 << 2);
+    
+    CCM_ANALOG->PLL_ARM &= ~0x7f;
+    CCM_ANALOG->PLL_ARM |= 88;
+    CCM_ANALOG->PLL_ARM |= (1 << 13);
+    
+    ze_u32_t value = CCM->CACRR;
+    value &= ~07;
+    value |= 0x01;
+    CCM->CACRR = value;
 
-    if ((CCM->CCSR >> 2) & 0x01 == 0)
-    {
-        CCM->CCSR &= ~(1 << 8);
-        CCM->CCSR |= (1 << 2);
-    }
-
-    CCM_ANALOG->PLL_ARM |= (88 | (1 << 13));
     CCM->CCSR &= ~(1 << 2);
 
-    reg = CCM_ANALOG->PFD_528;
-    reg &= ~(0x3F3F3F3F);
-    reg |= 27;                  // PFD0 = 352 MHz
-    reg |= 
+    // PLL2 PDF0 352MHz
+    CCM_ANALOG->PFD_528 &= ~0x3F;
+    CCM_ANALOG->PFD_528 |= 27;
+    CCM_ANALOG->PFD_528 &= ~(1 << 7);
 
+    // PLL2 PFD1 594MHz
+    CCM_ANALOG->PFD_528 &= ~(0X3F << 8);
+    CCM_ANALOG->PFD_528 |= (16 << 8);
+    CCM_ANALOG->PFD_528 &= ~(1 << 15);
+    // PLL2 PFD2 400MHz
+    CCM_ANALOG->PFD_528 &= ~(0X3F << 16);
+    CCM_ANALOG->PFD_528 |= (24 << 16);
+    CCM_ANALOG->PFD_528 &= ~(1 << 23);
+    // PLL2 PFD3 297MHz
+    CCM_ANALOG->PFD_528 &= ~(0X3F << 24);
+    CCM_ANALOG->PFD_528 |= (32 << 24);
+    CCM_ANALOG->PFD_528 &= ~(1 << 31);
+    // PLL3 PFD0 720MHz
+    CCM_ANALOG->PFD_480 &= ~0x3F;
+    CCM_ANALOG->PFD_480 |= 12;
+    CCM_ANALOG->PFD_480 &= ~(1 << 7);
+    // PLL3 PFD1 540MHz
+    CCM_ANALOG->PFD_480 &= ~(0x3F << 8);
+    CCM_ANALOG->PFD_480 |= (16 << 8);
+    CCM_ANALOG->PFD_480 &= ~(1 << 15);
+    // PLL3 PFD2 508.2MHz
+    CCM_ANALOG->PFD_480 &= ~(0x3F << 16);
+    CCM_ANALOG->PFD_480 |= (17 << 16);
+    CCM_ANALOG->PFD_480 &= ~(1 << 23);
+    // PLL3 PFD3 454.7MHz
+    CCM_ANALOG->PFD_480 &= ~(0x3F << 24);
+    CCM_ANALOG->PFD_480 |= (19 << 24);
+    CCM_ANALOG->PFD_480 &= ~(1 << 31);
+
+
+    // Set AHB clock 132MHz
+    CCM->CBCMR &= ~(0x03 << 18);
+    CCM->CBCMR |= (0x01 << 18);
+
+    CCM->CBCDR &= ~(1 << 25);
+    while (CCM->CDHIPR & (1 << 5));
+
+
+    CCM->CBCDR &= ~(3 << 8);
+    CCM->CBCDR |= 1 << 8;
+
+    CCM->CSCMR1 &= ~(1 << 6);
+    CCM->CSCMR1 &= ~(7 << 0);
 }
-*/
+
 static void periph_clock_enable(void)
 {
     CCM->CCGR0 = 0xFFFFFFFF;
@@ -38,6 +85,6 @@ static void periph_clock_enable(void)
 
 void clock_init(void)
 {
-
+    system_clock_init();
     periph_clock_enable();
 }
